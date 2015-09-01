3efbbd6b1ef739141d224edc839cc8bc264fb2d1
Caio Torres <caltorres@sp.ir7.com.br>
2015-09-01 20:07:13 -0300

    Moved ActiveRecord initialization to priori_data bootstrap file

--- config.ru
+++ config.ru
@@ -1,5 +1,3 @@
 require './lib/application'
 
-use ActiveRecord::ConnectionAdapters::ConnectionManagement
-
 run PrioriData::API
--- lib/priori_data.rb
+++ lib/priori_data.rb
@@ -1,3 +1,9 @@
 require 'active_record'
+require 'yaml'
+
+env = ENV["RACK_ENV"] || 'development'
+
+db_options = YAML.load(File.read('./config/database.yml'))[env]
+ActiveRecord::Base.establish_connection(db_options)
 
 Dir["#{Dir.pwd}/lib/**/*.rb"].each {|file| require file }
