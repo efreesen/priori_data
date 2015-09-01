f9844c6ed6412e3998327ad15949f494d8d50d52
Caio Torres <caltorres@sp.ir7.com.br>
2015-09-01 18:24:54 -0300

    Fixed specs

--- spec/integration/categories_spec.rb
+++ spec/integration/categories_spec.rb
@@ -1,6 +1,16 @@
 describe PrioriData::Integration::Categories do
   let(:instance) { described_class.new }
 
+  describe '.valid_categories' do
+    let(:list) { [36, 6000, 6001, 6002, 6003, 6004, 6005, 6006, 6007, 6008, 6009, 6010, 6011, 6012, 6013, 6014, 6015, 6016, 6017, 6018, 6020, 6021, 6022, 7001, 7002, 7003, 7004, 7005, 7006, 7007, 7008, 7009, 7010, 7011, 7012, 7013, 7014, 7015, 7016, 7017, 7018, 7019, 13001, 13002, 13003, 13004, 13005, 13006, 13007, 13008, 13009, 13010, 13011, 13012, 13013, 13014, 13015, 13017, 13018, 13019, 13020, 13021, 13023, 13024, 13025, 13026, 13027, 13028, 13029, 13030] }
+
+    subject { described_class.valid_categories }
+
+    it 'returns the list of valid ids' do
+      expect(subject).to eq(list)
+    end
+  end
+
   describe '.import' do
     subject do
       VCR.use_cassette("apple_categories_success") do
