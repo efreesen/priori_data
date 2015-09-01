5116551d4fca7255dc92ae9944b7c64ede2b1404
Caio Torres <caltorres@sp.ir7.com.br>
2015-09-01 20:06:34 -0300

    Added some relations

--- lib/models/app.rb
+++ lib/models/app.rb
@@ -1,2 +1,4 @@
 class App < ActiveRecord::Base
+  belongs_to :publisher, primary_key: :external_id
+  has_many :rankings
 end
--- lib/models/ranking.rb
+++ lib/models/ranking.rb
@@ -1,2 +1,3 @@
 class Ranking < ActiveRecord::Base
+  belongs_to :app, primary_key: :external_id
 end
